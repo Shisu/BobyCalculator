aac3b85740bd66ea0a7dc013fc0116a3dc8cce98
shisu <jenny-dittmann@gmx.de>
2018-11-19 22:08:58 +0100

    Add Logger, add first thoughts about how to use the config file

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -4,9 +4,9 @@ import file_processor.FileLoader;
 import view.Gui;
 
 public class Main {
-
 	public static void main(String[] args) {
-		FileLoader conf = new FileLoader();
-		Gui window = new Gui();
+		FileLoader config = new FileLoader();
+		
+		new Gui();
 	}
 }
--- src/main/java/file_processor/FileLoader.java
+++ src/main/java/file_processor/FileLoader.java
@@ -7,18 +7,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 public class FileLoader {
 
+	private static Logger log = Logger.getRootLogger();
+	
 	private static final String conf = "src/main/resources/config/configuration_file.config";
 	private List<String> records;
+	private String databasePath;
 	
 	public FileLoader() {
-		File confFile = new File(conf);
 		try {
 			loadFile(conf);
 			parseFile();
 		} catch (IOException e) {
-			// TODO Log
+			System.out.println("test");
+			log.error(e);
 		}
 	}
 	
@@ -29,10 +34,11 @@ public class FileLoader {
 	private void loadFile(String path) throws IOException{
 		records = new ArrayList<String>();
 		BufferedReader reader = new BufferedReader(new FileReader(path));
+		String line;
 		try {
-			while(true){
-				records.add(reader.readLine());				
-			}
+			while((line = reader.readLine()) != null){
+				records.add(line);
+			};
 		} finally{
 			if(reader != null) {
 				reader.close();
@@ -42,7 +48,11 @@ public class FileLoader {
 
 	private void parseFile() {
 		// TODO trigger process file here
-		
+		//Get databasePath here if its from Config
 	}
 
+	public String getDatabasePath(){
+		return databasePath;
+	}
+	
 }
