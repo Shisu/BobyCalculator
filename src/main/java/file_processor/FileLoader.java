be53664363db3c1984f3e5843567f9f25c8adf44
shisu <jenny-dittmann@gmx.de>
2018-11-21 21:38:14 +0100

    Change style of config-file and load databasefile-path

--- src/main/java/file_processor/FileLoader.java
+++ src/main/java/file_processor/FileLoader.java
@@ -1,22 +1,21 @@
 package file_processor;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
 public class FileLoader {
 
 	private static Logger log = Logger.getRootLogger();
-	
+
 	private static final String conf = "src/main/resources/config/configuration_file.config";
-	private List<String> records;
+	private Map<String, String> records;
 	private String databasePath;
-	
+
 	public FileLoader() {
 		try {
 			loadFile(conf);
@@ -26,21 +25,25 @@ public class FileLoader {
 			log.error(e);
 		}
 	}
-	
+
 	public FileLoader(String dataFile) {
 		// TODO Load "Database"-File
 	}
 
-	private void loadFile(String path) throws IOException{
-		records = new ArrayList<String>();
+	private void loadFile(String path) throws IOException {
+		records = new HashMap<>();
 		BufferedReader reader = new BufferedReader(new FileReader(path));
 		String line;
 		try {
-			while((line = reader.readLine()) != null){
-				records.add(line);
-			};
-		} finally{
-			if(reader != null) {
+			while ((line = reader.readLine()) != null) {
+				String[] tempArray = line.split("=");
+				records.put(tempArray[0].trim(), tempArray[1].trim());
+			}
+			if (!records.isEmpty()) {
+				databasePath = records.get("dataFolder");
+			}
+		} finally {
+			if (reader != null) {
 				reader.close();
 			}
 		}
@@ -48,11 +51,11 @@ public class FileLoader {
 
 	private void parseFile() {
 		// TODO trigger process file here
-		//Get databasePath here if its from Config
+		// Get databasePath here if its from Config
 	}
 
-	public String getDatabasePath(){
+	public String getDatabasePath() {
 		return databasePath;
 	}
-	
+
 }
